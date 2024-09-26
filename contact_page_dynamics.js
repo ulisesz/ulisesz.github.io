398e94abd7f83962259b29341715c2a8433ada61
Ulises Zaragoza <zaragozaulises@gmail.com>
2024-09-25 18:05:10 -0700

    Added basic support for contact page

--- contact_page_dynamics.js
+++ contact_page_dynamics.js
@@ -1,8 +1,13 @@
 var g_terminalCharacter = "|";
-var g_aboutPageText = {};
-var TEXT_FIELD = 0;
+var g_pageText = {};
+var CONTACT_FIELD = 0;
 var BLINK = g_outputType.BLINK;
 var TEXT = g_outputType.TEXT;
+
+function GetPageObject()
+{
+	return document.querySelector("#contact_page");
+}
 
 function RenderPage()
 {
@@ -12,8 +17,8 @@ function RenderPage()
 
 function LoadText()
 {
-	g_aboutPageText["text1"] = "I wanted some practice creating websites & also needed a place to host current/future projects of mine.";
-	g_aboutPageText["text2"] = " This site will continue to be a work in progress, but its purposes will remain the same.";
+	g_pageText["text1"] = "contact:";
+	g_pageText["text2"] = " https://www.linkedin.com/in/ulises-zaragoza ";
 }
 
 async function DisplayText()
@@ -24,15 +29,17 @@ async function DisplayText()
 
 async function RenderText()
 {
-	var textField = document.querySelector("#about_page");
+	var textField = GetPageObject();
 	if (textField)
 	{
-		var field = textField.children[TEXT_FIELD];
+		var field = textField.children[CONTACT_FIELD];
 		QueueOutput(field, BLINK, g_terminalCharacter);
 		QueueOutput(field, BLINK, g_terminalCharacter);
-		QueueOutput(field, TEXT, g_aboutPageText["text1"]);
+		QueueOutput(field, TEXT, g_pageText["text1"]);
 		QueueOutput(field, BLINK, g_terminalCharacter);
-		QueueOutput(field, TEXT, g_aboutPageText["text2"]);
+		
+		field = textField.children[CONTACT_FIELD];
+		QueueOutput(field, TEXT, g_pageText["text2"]);
 		await RenderOutput();
 	}	
 	return new Promise((resolve) => {
@@ -42,10 +49,10 @@ async function RenderText()
 
 async function InfiniteBlink()
 {
-	var textField = document.querySelector("#about_page");
+	var textField = GetPageObject();
 	if (textField)
 	{
-		var field = textField.children[TEXT_FIELD];
+		var field = textField.children[CONTACT_FIELD];
 		while (true)
 		{
 			QueueOutput(field, BLINK, g_terminalCharacter);
@@ -58,3 +65,5 @@ async function InfiniteBlink()
 }
 
 RenderPage();
+
+
