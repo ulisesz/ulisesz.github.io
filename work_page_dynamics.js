50815cccb2f6c06566cd4e45a5754f7468b466f3
Ulises Zaragoza <zaragozaulises@gmail.com>
2024-10-14 22:48:09 -0700

    Added basic initial text to work page

--- work_page_dynamics.js
+++ work_page_dynamics.js
@@ -1,6 +1,7 @@
 var g_terminalCharacter = "|";
 var g_workPageText = {};
 var TEXT_FIELD = 0;
+var P1_FIELD = 1;
 var BLINK = g_outputType.BLINK;
 var TEXT = g_outputType.TEXT;
 var g_workPageId = "#work_page";
@@ -14,15 +15,18 @@ function RenderPage()
 function LoadText()
 {
 	g_workPageText["text1"] = "Ulises Zaragoza";
+	g_workPageText["text2"] = "Product Owner for Intel's Power/Thermal framework software with +90% adoption rate on mobile platforms by industry leading OEM/ODMs."
+	g_workPageText["text3"] = " Passionate for building high quality software and driving team success through collaboration, demonstration & coordination.";
 }
 
 async function DisplayText()
 {
-	await RenderText();
+	await RenderHeader();
+	await RenderP1();
 	await InfiniteBlink();
 }
 
-async function RenderText()
+async function RenderHeader()
 {
 	var textField = document.querySelector(g_workPageId);
 	if (textField)
@@ -31,6 +35,25 @@ async function RenderText()
 		QueueOutput(field, BLINK, g_terminalCharacter);
 		QueueOutput(field, BLINK, g_terminalCharacter);
 		QueueOutput(field, TEXT, g_workPageText["text1"]);
+		QueueOutput(field, BLINK, g_terminalCharacter);
+		await RenderOutput();
+	}	
+	return new Promise((resolve) => {
+        resolve("rendered");
+	});
+}
+
+async function RenderP1()
+{
+	var textField = document.querySelector(g_workPageId);
+	if (textField)
+	{
+		var field = textField.children[P1_FIELD];
+		QueueOutput(field, BLINK, g_terminalCharacter);
+		QueueOutput(field, BLINK, g_terminalCharacter);
+		QueueOutput(field, TEXT, g_workPageText["text2"]);
+		QueueOutput(field, BLINK, g_terminalCharacter);
+		QueueOutput(field, TEXT, g_workPageText["text3"]);
 		await RenderOutput();
 	}	
 	return new Promise((resolve) => {
@@ -43,7 +66,7 @@ async function InfiniteBlink()
 	var textField = document.querySelector(g_workPageId);
 	if (textField)
 	{
-		var field = textField.children[TEXT_FIELD];
+		var field = textField.children[P1_FIELD];
 		while (true)
 		{
 			QueueOutput(field, BLINK, g_terminalCharacter);
